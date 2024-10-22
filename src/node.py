e5264e3603dc8e773f6f1729defd3a86344e936c
urbanski220 <jakub.urbanski115@gmail.com>
2024-10-22 17:39:59 +0200

    added primitive node connections

--- src/node.py
+++ src/node.py
@@ -7,6 +7,8 @@ import sys
 from time import time 
 
 from flask import Flask, request
+import requests
+
 
 class Block():
     def __init__(self, ind: int, prev_hash: str, data: str) -> None:
@@ -35,6 +37,8 @@ class Block_chain():
 
 app = Flask(__name__)
 block_chain = Block_chain()
+Nodes = {}
+node_name = ''
 
 @app.route('/')
 def index() -> str:  
@@ -44,16 +48,51 @@ def index() -> str:
 def getBlocks() -> str:  
     return f"{block_chain.chain}\n"
 
+@app.post('/nodes')
+def post_nodes():
+    new_node = request.json
+    name = new_node['name']
+    if name not in Nodes:
+        Nodes[name] = {}
+        Nodes[name]['name'] = name
+        Nodes[name]['url'] = new_node['url']
+    return "\n", 201
+
+@app.get('/nodes')
+def get_nodes():
+    return Nodes
+
+def connect(url):
+    r = requests.get(url+'/nodes')
+    nodes = r.json()
+    for name in nodes.keys():
+        if name != node_name:
+           Nodes[name] = {}
+           Nodes[name]['name'] = name
+           Nodes[name]['url'] = nodes[name]['url']
+           requests.post(nodes[name]['url']+'/nodes', json=Nodes[node_name])
+
 def main(app: Flask) -> int:
+    global node_name
     parser = argparse.ArgumentParser()
     parser.add_argument('--init', help='Initialise the first node.', action='store_true')
     parser.add_argument('--join', help='Create a new node. Please specify the port of the node it should be connected to')
+    parser.add_argument('--port', help='Please specify the port number of the node', type=int)
     args = parser.parse_args()
-    
+
+    node_name = str(args.port)
+
+    Nodes[node_name] = {}
+    Nodes[node_name]['name'] = node_name
+    Nodes[node_name]['url'] = "http://127.0.0.1:"+node_name
+
     if args.init:
-        app.run(host='127.0.0.1', port=3001)
+        print("Starting node "+node_name+" on port "+node_name)
+        app.run(host='127.0.0.1', port=node_name)
     elif args.join:
-        pass
+        print("Starting node "+node_name+" on port "+node_name)
+        connect('http://127.0.0.1:'+str(args.join))
+        app.run(host='127.0.0.1', port=node_name)
     else:
         return 1
     return 0
